0d4f6320cb1325af339fb5e5cc6b53bf1418fe9f
ywb.hs <ywb.hs@qq.com>
2018-11-09 00:23:22 +0800

    Initial commit

---
+++ aline_invasion.py
@@ -0,0 +1,35 @@
+import sys
+
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from ship import Ship
+import game_functions as g_func
+
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    g_func.create_fleet(ai_settings, screen, aliens)
+
+    # 开始游戏的主循环
+    while True:
+
+        g_func.check_envents(ai_settings, screen, ship, bullets)
+        ship.update()
+        g_func.update_bullets(bullets)
+        g_func.updage_screen(ai_settings, screen, ship, aliens, bullets)
+
+
+run_game()
